1e25322d610f750f5b24c531c18705dc012ebcf3
Vladimir Costa Bezerra <vladimir.cbezerra@gmail.com>
2021-03-15 19:14:22 -0300

    primeiro exercicio

---
+++ 1_exercicio.js
@@ -0,0 +1,12 @@
+/* Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a
+função) e com ponto de exclamação "!" no final.
+Exemplos:
+cumprimentar("Leonardo") // retornará "Olá, Leonardo!"
+cumprimentar("Maria") // retornará "Olá, Maria!"
+*/
+
+const cumprimento = (nome) =>  
+{
+    return `Olá, ${nome}!`
+}
+console.log(cumprimento(Maria))
