d760c146ad675bae309d2bb9c6c49dab4ffba0c4
Vladimir Costa Bezerra <vladimir.cbezerra@gmail.com>
2021-03-22 21:39:40 -0300

    revi

---
+++ 13_exercicio.js
@@ -0,0 +1,13 @@
+/*Crie uma função que receba um array de elementos e retorne um array somente com os números presentes no
+array recebido como parâmetro.
+Exemplos:
+filtrarNumeros(["Javascript", 1, "3", "Web", 20]) // retornará [1, 20]
+filtrarNumeros(["a", "c"]) // retornará []
+ */
+
+const filtrarNumeros = (filtro) => {
+    
+
+}
+
+console.log(filtrarNumeros(["Javascript", 1, "3", "Web", 20]))
