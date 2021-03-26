024b69cd6ed75152f06d11fea9900d5770bcac85
Vladimir Costa Bezerra <vladimir.cbezerra@gmail.com>
2021-03-26 19:03:03 -0300

    17_exercicio

---
+++ 17_exercicio.js
@@ -0,0 +1,22 @@
+/* Escreva uma função que receba um array de números e retornará a soma de todos os números desse array.
+Exemplos:
+somarNumeros([10, 10, 10]) // retornará 30
+somarNumeros([15, 15, 15, 15]) // retornará 60
+ 
+const somarNumeros = [10,10,10,10,10]
+const somaReduce = somarNumeros.reduce(function(valorAcumulativo,valorArray){
+    return valorAcumulativo + valorArray
+},0)
+console.log(somaReduce)*/
+
+
+const somarNumeros = (valor) => {
+    let soma = 0
+    for (let i = 0; i < valor.length; i++){
+        soma = soma + valor[i]
+    }
+    return  soma
+}
+console.log(somarNumeros([10,10,10,10]))
+
+
