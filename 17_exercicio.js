0f6e3f6d8cde9f32dec7732374f37de1e0358dc0
Vladimir Costa Bezerra <vladimir.cbezerra@gmail.com>
2021-03-26 21:34:31 -0300

    17_exercício

--- 17_exercicio.js
+++ 17_exercicio.js
@@ -2,14 +2,17 @@
 Exemplos:
 somarNumeros([10, 10, 10]) // retornará 30
 somarNumeros([15, 15, 15, 15]) // retornará 60
- 
+ */
 const somarNumeros = [10,10,10,10,10]
 const somaReduce = somarNumeros.reduce(function(valorAcumulativo,valorArray){
-    return valorAcumulativo + valorArray
-},0)
-console.log(somaReduce)*/
+    valorAcumulativo.soma += valorArray
+    return valorAcumulativo
+    //return {soma:valorAcumulativo.soma + valorArray}
+},{soma:0})
+console.log(somaReduce)
 
 
+/*
 const somarNumeros = (valor) => {
     let soma = 0
     for (let i = 0; i < valor.length; i++){
@@ -18,5 +21,5 @@ const somarNumeros = (valor) => {
     return  soma
 }
 console.log(somarNumeros([10,10,10,10]))
-
+*/
 
